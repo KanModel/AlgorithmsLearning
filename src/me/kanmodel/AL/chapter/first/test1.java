ee46c5607d9ccb36314b857b177f59c6966146b0
Kan_Model <kgdwhsk@gmail.com>
2017-06-27 12:54:58 +0800

    Pratice & test

--- src/me/kanmodel/AL/chapter/first/test1.java
+++ src/me/kanmodel/AL/chapter/first/test1.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import static java.security.AccessController.doPrivileged;
 import static me.kanmodel.AL.assist.getRandomArray;
 import static me.kanmodel.AL.assist.showArray;
+import static me.kanmodel.AL.chapter.first.test1.doAdd;
 
 /**
  * Created with IntelliJ IDEA.
@@ -198,39 +199,127 @@ public class test1 {
             StdDraw.filledRectangle(x, y, rw, rh);
         }
     }
+
+    /**
+     *
+     * @param a 加数字符串1
+     * @param b 加数字符串2
+     * @return 结果字符串
+     * 分析：
+     * 1、取得两个字符串的长度
+     * 2、把两个的长度做比较，并得出较长的长度，及较短的长度
+     * 3、把长度较短的加数字符串，在左面补0，使之与较长的字符串一样长
+     * 4、从最高位，一个个数的取出来相加，当然首先得转换为整型
+     * 5、设置进位，如果两个数相加及加上进位大于等于10，并且这不是最左边一个字符相加，相加结果等于
+     *    （取出1＋取出2＋进位）－10，并把进位设为1；如果没有大于10，就把进位设为0，如些循环，把
+     *    相加的结果以字符串的形式结合起来，就得到最后的结果
+     *
+     *    转自http://blog.csdn.net/fenglibing/article/details/1756773
+     */
+    public static String doAdd(String a,String b)
+    {
+        String str="";
+        int lenA=a.length();
+        int lenB=b.length();
+        int maxLen=(lenA>lenB) ? lenA : lenB;
+        int minLen=(lenA<lenB) ? lenA : lenB;
+        String strTmp="";
+        for(int i=maxLen-minLen;i>0;i--)
+        {
+            strTmp+="0";
+        }
+        //把长度调整到相同
+        if(maxLen==lenA)
+        {
+            b=strTmp+b;
+        }else
+            a=strTmp+a;
+        int JW=0;//进位
+        for(int i=maxLen-1;i>=0;i--)
+        {
+            int tempA=Integer.parseInt(String.valueOf(a.charAt(i)));
+            int tempB=Integer.parseInt(String.valueOf(b.charAt(i)));
+            int temp;
+            if(tempA+tempB+JW>=10 && i!=0)
+            {
+                temp=tempA+tempB+JW-10;
+                JW=1;
+            }
+            else
+            {
+                temp=tempA+tempB+JW;
+                JW=0;
+            }
+            str=String.valueOf(temp)+str;
+        }
+        return str;
+    }
 }
 
 /**
  * 19斐波那契
  */
 class Fibonacci{
-    private static ArrayList<Integer> list = new ArrayList<>();
-    public static long F(int N){//todo 斐波那契数列完善
-        if (N < list.size()){
-            if (N - 1 <= 0) {
-                return 0;
-            }
-            return list.get(N - 1) + list.get(N - 2);
+    private static ArrayList<Long> list = new ArrayList<>();
+    private static ArrayList<String> slist = new ArrayList<>();
+
+    public static String F2(int N){
+        if (N < slist.size()){
+            return slist.get(N);
         }else {
             if (N == 0) {
-                list.add(0);
+                slist.add(String.valueOf(0));
+                return slist.get(0);
+            }
+            if (N == 1){
+                slist.add(String.valueOf(1));
+                return slist.get(1);
+            }
+        }
+//        slist.add((F2(N - 1) + F2(N - 2)));
+        slist.add(doAdd(F2(N - 1), F2(N - 2)));
+        return slist.get(N);
+    }
+
+    /**
+     * 第一个版本，会超出Long类型上限
+     * @param N 斐波那契数列第N个数
+     * @return 斐波那契数列第N个数
+     */
+    public static long F1(int N){
+        if (N < list.size()){
+            return list.get(N);
+        }else {
+            if (N == 0) {
+                list.add((long) 0);
                 return 0;
             }
             if (N == 1){
-                list.add(1);
+                list.add((long) 1);
                 return 1;
             }
         }
-        return F(N - 1) + F(N - 2);
+        list.add((F1(N - 1) + F1(N - 2)));
+        return list.get(N);
     }
+
     public static long FO(int N){
         if (N == 0) return 0;
         if (N == 1) return 1;
         return FO(N - 1) + FO(N - 2);
     }
+
     public static void main(String[] args){
         for (int N = 0; N < 100; N++)
-//            StdOut.println(N + " " + F(N));
-            StdOut.println(N + " " + FO(N));
+            System.out.println(N + " " + F2(N));
+//            System.out.println(N + " " + F1(N));
+//            StdOut.println(N + " " + FO(N));
+    }
+}
+
+class casualTest{
+    public static void main(String args[]){
+        String s = doAdd("123453243455535634535252345234677576252241234123523453664563634", "123453243455535634535252345234677576252241234123523453664563634");
+        System.out.println(s);
     }
 }
