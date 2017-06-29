44581cba6330548a0f875ec744577a30ad60e09a
Kan_Model <kgdwhsk@gmail.com>
2017-06-29 16:29:02 +0800

    Pratice & test

--- src/me/kanmodel/AL/chapter/first/test1.java
+++ src/me/kanmodel/AL/chapter/first/test1.java
@@ -1,7 +1,7 @@
 package me.kanmodel.AL.chapter.first;
 
-import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import org.jetbrains.annotations.Contract;
@@ -64,6 +64,14 @@ public class test1 {
         showArray(histogram(getRandomArray(20, false, true, 10), 10));
         //16
         System.out.println(exRl(6));
+        //18
+        System.out.println(mystery(2, 25));
+        System.out.println(mystery(3, 11));
+        //19
+        Fibonacci.main(args);
+        //20 todo 非递归使用
+        System.out.println(lnN(3));
+        //21
         //anything else
         System.out.println(-.01);
 //        stdDrawTest1();
@@ -78,6 +86,21 @@ public class test1 {
         System.out.println(bl.length + " " + bl[1].length);
         System.out.println(doPrivileged((PrivilegedAction<String>) () -> System.getProperty("user.name")));
         new Thread( () -> System.out.println("In Java8, Lambda expression rocks !!") ).start();
+    }
+
+    public static double lnN(int n){
+        int result = 1;
+        for (int i = 1; i < n+1; i++){
+            result *= i;
+        }
+        System.out.println(result);
+        return Math.log(result);
+    }
+
+    public static int mystery(int a, int b){
+        if (b == 0) return 0;
+        if (b % 2 == 0) return mystery(a + a, b / 2);
+        return mystery(a + a, b / 2) + a;
     }
 
     public static int[] histogram(int[] a, int M){
@@ -314,12 +337,127 @@ class Fibonacci{
             System.out.println(N + " " + F2(N));
 //            System.out.println(N + " " + F1(N));
 //            StdOut.println(N + " " + FO(N));
+//        System.out.println(F2(1005));
     }
 }
 
 class casualTest{
     public static void main(String args[]){
-        String s = doAdd("123453243455535634535252345234677576252241234123523453664563634", "123453243455535634535252345234677576252241234123523453664563634");
+        String s = doAdd("723453243455535634535252345234677576252241234123523453664563634", "623453243455535634535252345234677576252241234123523453664563634");
         System.out.println(s);
+        System.out.println(25 / 2);
+        System.out.println(3 / 2);
+        System.out.println(1 / 2);
+
+        double sum = 0;
+        int cnt = 0;
+        while (!StdIn.isEmpty()){
+            sum += StdIn.readDouble();
+            cnt++;
+        }
+        double avg = sum / cnt;
+        StdOut.printf("平均值： %.5f\n", avg);
     }
 }
+
+//import java.util.Arrays;
+/**
+ * 矩阵的直接三角分解 ，调用示例：
+ *
+ * DirectDecomposition dd = new DirectDecomposition(data);//data为一个二维double数组，代替一个矩阵
+ *
+ * double[][] l = dd.getL();//获取L
+ *
+ * double[][] u = dd.getU();//获取U
+ *
+ * @author 清雨季
+ */
+class DoolittleDecomposition {
+    private double[][] data;
+    private double[][] l;
+    private double[][] u;
+    private int n;
+    /**
+     * 创建一个n阶的矩阵
+     *
+     * @param data
+     */
+    public DoolittleDecomposition(double[][] data) {
+        if (data == null || data.length == 0 || data.length != data[0].length) {
+            throw new RuntimeException("不是一个方阵");
+        }
+        this.data = data;
+        n = data.length;
+        l = new double[n][n];
+        u = new double[n][n];
+        countLU();
+    }
+    protected void countLU() {
+        for (int i = 0; i < n; i++) {// 第一步，计算L的第一列和U的第一行：U1i=A1i,Li1=Ai1/U11
+            u[0][i] = data[0][i];
+            l[i][0] = data[i][0] / u[0][0];
+        }
+        //计算U的第r行，L的第r列元素
+        //uri=ari-Σ(k=1->r-1)lrkuki    (i=r,r+1,...,n)
+        //lir=air-Σ(k=1->r-1)likukr    (i=r+1,r+2,...,n且r≠n)
+        for (int r = 1; r < n; r++) {
+            for (int i = r; i < n; i++) {
+                u[r][i] = data[r][i] - sumLrkUki(r, i);
+                if(i==r) l[r][r]=1;
+                else if(r==n) l[n][n]=1;
+                else l[i][r] = (data[i][r] - sumLikUkr(r, i)) / u[r][r];
+            }
+        }
+    }
+    /**
+     * 求和：Lrk*Uki 对k求和：1<=k<=r-1
+     *
+     * @param r
+     * @param i
+     * @return
+     */
+    private double sumLrkUki(int r, int i) {
+        double re = 0.0;
+        for (int k = 0; k < r; k++) {
+            re += l[r][k] * u[k][i];
+        }
+        return re;
+    }
+    private double sumLikUkr(int r, int i) {
+        double re = 0.0;
+        for (int k = 0; k < r; k++) {
+            re += l[i][k] * u[k][r];
+        }
+        return re;
+    }
+    public double[][] getData() {
+        return data;
+    }
+    public double[][] getL() {
+        return l;
+    }
+    public double[][] getU() {
+        return u;
+    }
+    public static void main(String[] args) {
+        double[][] data= {
+                {1,2,6},
+                {2,5,15},
+                {6,15,46},
+        };
+        DoolittleDecomposition dd = new DoolittleDecomposition(data);
+        double[][] l = dd.getL();
+        double[][] u = dd.getU();
+        int n = l.length;
+        System.out.println("L阵：");
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(u[i]));
+        }
+        System.out.println("---------------------");
+        System.out.println("U阵：");
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(l[i]));
+        }
+    }
+}
+
